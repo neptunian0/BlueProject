79e0292b93d9d1d731f44b5ec24febdc02f05f05
neptunian0 <the-neptunian@hotmail.com>
2016-09-30 16:20:32 +0100

    Work in progress for only showing greenbank account and transactions

--- app/src/main/java/com/lloydtucker/blueproject/MainActivity.java
+++ app/src/main/java/com/lloydtucker/blueproject/MainActivity.java
@@ -30,22 +30,16 @@ import okhttp3.HttpUrl;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     static final String HTTPS = "https";
-    static final String BLUE_URI = "bluebank.azure-api.net";
     static final String GREEN_URI = "greenbank.azure-api.net";
-    static final String BLUE_API = "api";
     static final String GREEN_PCA = "PCA";
-    static final String BLUE_VERSION = "v0.6.3";
     static final String ACCOUNTS = "accounts";
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    static float sAnimatorScale = 2;
-    //public final static String EXTRA_MESSAGE = "com.lloydtucker.testproject.CONTACTS";
     private ListView listView;
     private TextView greetingView, greetingDateView;
     private ProgressBar mainProgressBar;
     private ArrayAdapter<Accounts> adapter;
     private RelativeLayout greetingHeader;
-    static View selectedAccount;
     String response;
 
     /*
@@ -84,18 +78,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private static final String TAG_ACCOUNTS = "accounts";
     private static final String TAG_TRANSACTIONS = "transactions";
 
-    /*
-    * ANIMATION TAGS
-    */
-    static final String TAG_ACCOUNT_TOP = "accountTop";
-    static final String TAG_ACCOUNT_LEFT = "accountLeft";
-
-    //JSON Array
-    //JSONArray contacts = new JSONArray();
-
     //Contacts array
-    Customers[] customers = new Customers[2];
-    Accounts[] accounts = new Accounts[3];
+    Customers[] customers = new Customers[1];
+    Accounts[] accounts = new Accounts[1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,106 +94,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mainProgressBar = (ProgressBar) findViewById(R.id.mainProgressBar);
 
         //Make the API call
-        getCustomers();
         getGreenCustomers();
-    }
-
-    private void getCustomers() {
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                mainProgressBar.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    response = ApiCall.GET(buildURL());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "ERROR: IOException");
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                //Parse the response string here
-                try {
-                    JSONArray jsonArr = new JSONArray(response);
-
-                    // looping through All Contacts
-                    //should be i < jsonArr.length(), but only want 10 customers
-                    for (int i = 0; i < jsonArr.length() && i < 10; i++) {
-                        JSONObject c = jsonArr.getJSONObject(i);
-                        Customers cus = new Customers();
-
-                        cus.setId(c.getString(TAG_ID));
-                        cus.setGivenName(c.getString(TAG_GIVEN_NAME));
-                        cus.setFamilyName(c.getString(TAG_FAMILY_NAME));
-                        cus.setTown(c.getString(TAG_TOWN));
-                        cus.setPostCode(c.getString(TAG_POST_CODE));
-
-                        customers[i] = cus;
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "ERROR: JSONException");
-                }
-                if (customers[0] != null) {
-                    getAccounts();
-                }
-            }
-        }.execute();
-    }
-
-    /*
-    * Get Accounts
-    */
-    public void getAccounts() {
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-                try {
-                    response = ApiCall.GET(buildURL(customers[0].getId()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "ERROR: IOException");
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                try {
-                    JSONArray jsonArr = new JSONArray(response);
-
-                    // looping through All Accounts for this Customer
-                    //should be i < jsonArr.length(), but only want 10 customers
-                    for (int i = 0; i < jsonArr.length() && i < 10; i++) {
-                        JSONObject c = jsonArr.getJSONObject(i);
-                        Accounts acc = new Accounts();
-
-                        acc.setId(c.getString(TAG_ID));
-                        acc.setCustId(c.getString(TAG_CUSTOMER_ID));
-                        acc.setSortCode(c.getString(TAG_SORT_CODE));
-                        acc.setAccountNumber(c.getString(TAG_ACCOUNT_NUMBER));
-                        acc.setAccountType(c.getString(TAG_ACCOUNT_TYPE));
-                        acc.setAccountFriendlyName(c.getString(TAG_ACCOUNT_FRIENDLY_NAME));
-                        acc.setAccountBalance(c.getDouble(TAG_ACCOUNT_BALANCE));
-                        acc.setAccountCurrency(c.getString(TAG_ACCOUNT_CURRENCY));
-
-                        accounts[i] = acc;
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "ERROR: JSONException");
-                }
-            }
-        }.execute();
     }
 
     private void getGreenCustomers() {
@@ -223,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             protected Void doInBackground(Void... params) {
                 try {
                     response = GreenApiCall.GET(buildGreenURL());
+                    Log.d("Response", response);
                 } catch (IOException e) {
                     e.printStackTrace();
                     Log.d(TAG, "ERROR: IOException");
@@ -250,13 +137,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         cus.setPostCode(c.getString(TAG_POST_CODE));
 
                         //assumes only one customer input from BlueBank
-                        customers[i+1] = cus;
+                        customers[i] = cus;
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
                     Log.d(TAG, "ERROR: JSONException");
                 }
-                if (customers[1] != null) {
+                if (customers[0] != null) {
                     getGreeting(greetingView, greetingDateView);
                     greetingHeader.setAlpha(1);
                     getGreenAccounts();
@@ -319,7 +206,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     acc.setAccountBalance(jsonObj.getDouble(TAG_ACCOUNT_BALANCE));
                     acc.setAccountCurrency(jsonObj.getString(TAG_ACCOUNT_CURRENCY));
 
-                    accounts[2] = acc; //assumes two accounts from BlueBank
+                    accounts[0] = acc; //assumes two accounts from BlueBank
 
                     //render the loaded information
                     mainProgressBar.setVisibility(View.GONE);
@@ -332,29 +219,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 }
             }
         }.execute();
-    }
-
-
-    public static HttpUrl buildURL() {
-        return new HttpUrl.Builder()
-                .scheme(HTTPS)
-                .host(BLUE_URI)
-                .addPathSegment(BLUE_API)
-                .addPathSegment(BLUE_VERSION)
-                .addPathSegment(TAG_CUSTOMERS)
-                .build();
-    }
-
-    public static HttpUrl buildURL(String s) {
-        return new HttpUrl.Builder()
-                .scheme(HTTPS)
-                .host(BLUE_URI)
-                .addPathSegment(BLUE_API)
-                .addPathSegment(BLUE_VERSION)
-                .addPathSegment(TAG_CUSTOMERS)
-                .addPathSegment(s)
-                .addPathSegment(TAG_ACCOUNTS)
-                .build();
     }
 
     public static HttpUrl buildGreenURL() {
@@ -407,7 +271,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         } else if (timeOfDay >= 17 && timeOfDay < 24) {
             greeting += "Good evening, ";
         }
-        greeting += customers[1].getGivenName();
+        greeting += customers[0].getGivenName();
         g.setText(greeting);
 
         //set the date at the bottom of the header greeting
@@ -416,17 +280,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     public void onItemClick(AdapterView<?> l, View v, int position, long id) {
-        if(!accounts[position].getSortCode().equals("606162")) {
-            // Then you start a new Activity via Intent
-            Intent intent = new Intent(MainActivity.this, TransactionsActivity.class);
+        // Then you start a new Activity via Intent
+        Intent intent = new Intent(MainActivity.this, TransactionsActivity.class);
 
-            intent.putExtra(TAG_ID, accounts[position].getId());
-            intent.putExtra(TAG_ACCOUNT_FRIENDLY_NAME, accounts[position].getAccountFriendlyName());
-            intent.putExtra(TAG_ACCOUNT_NUMBER, accounts[position].getAccountNumber());
-            intent.putExtra(TAG_SORT_CODE, accounts[position].getSortCode());
-            intent.putExtra(TAG_ACCOUNT_BALANCE, accounts[position].getAccountBalance());
-            intent.putExtra(TAG_CUSTOMER_ID, customers[0].getId());
-            startActivity(intent);
-        }
+        intent.putExtra(TAG_ID, accounts[position].getId());
+        intent.putExtra(TAG_ACCOUNT_FRIENDLY_NAME, accounts[position].getAccountFriendlyName());
+        intent.putExtra(TAG_ACCOUNT_NUMBER, accounts[position].getAccountNumber());
+        intent.putExtra(TAG_SORT_CODE, accounts[position].getSortCode());
+        intent.putExtra(TAG_ACCOUNT_BALANCE, accounts[position].getAccountBalance());
+        intent.putExtra(TAG_CUSTOMER_ID, customers[0].getId());
+        startActivity(intent);
     }
 }
